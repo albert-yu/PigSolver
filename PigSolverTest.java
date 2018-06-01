7c8878f3f2b4a96dbf0921b901f9918bafb43d91
Albert Yu <albert-yu@users.noreply.github.com>
2018-06-01 10:55:04 -0500

    Update PigSolverTest.java

--- PigSolverTest.java
+++ PigSolverTest.java
@@ -2,20 +2,21 @@ import java.lang.Math;
 
 public class PigSolverTest
 {
-	public static void main(String[] args)
-	{
-		PigSolver solver = new PigSolver(4, 2, 2);
+    public static void main(String[] args)
+    {
+        PigSolver solver = new PigSolver(4, 2, 2);
 
-		Double[][] sampleArray = {{1.0, 1.0, 1.0, 1.0}, {1.0, 1.0, 1.0, 0.0}, {1.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0}};
+        Double[][] sampleArray = {{1.0, 1.0, 1.0, 1.0}, {1.0, 1.0, 1.0, 0.0}, {1.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0}};
 
-		for (int i = 0; i < 4; i ++)
-		{
-			solver.setExpectedWins(i, sampleArray);
-		}
+        for (int i = 0; i < 4; i ++)
+        {
+            solver.setExpectedWins(i, sampleArray);
+        }
 
-		System.out.println(solver.converged());  // true (need to set this to public in PigSolver.java)
-		System.out.println(1.0000000000000001 == 1.0000000000000002);  // false
-		System.out.println(1.00000000000000001 == 1.00000000000000002);  // true
-		System.out.println(Math.max(4, 6)); // 6
-	}
+        System.out.println(solver.converged());  // true (need to set this to public in PigSolver.java)
+        System.out.println(1.0000000000000001 == 1.0000000000000002);  // false
+        System.out.println(1.00000000000000001 == 1.00000000000000002);  // true
+        
+        System.out.println(Math.max(4, 6)); // 6
+    }
 }
